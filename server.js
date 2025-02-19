55e3473a8831d04134ae780dc1f2a71714fce302
sherzod992 <sherzodbek.sharobiddinov1@gmail.com>
2025-02-19 16:49:31 +0900

    Your commit message

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
 console.log('Server-side code running');
-// npm install express 
+
 const express = require('express');
+const app = express();
 const http = require('http');
+const path = require('path');
 
-const app = express(); // express objekti app o'zgaruvchisiga joylanadi
-// expressga oid kodlar yoziladi
-app.use(express.static('public')); // public papkasidagi fayllarni serverga joylash uchun
-app.use(express.json()); // json fayllarini serverga joylash uchun
-app.use(express.urlencoded({ extended: true })); // urlencoded fayllarini serverga joylash uchun
+// Static fayllar uchun public papkani ko'rsatish
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.set("views", "views"); // views papkasini serverga joylash uchun
-app.set('view engine', 'ejs'); // ejs fayllarini serverga joylash uchun
+// EJS ko'rinishini sozlash
+app.set("views", "views");
+app.set('view engine', 'ejs');
 
-
+// Asosiy sahifa
 app.get('/', (req, res) => {
-    res.end('<h1 style="background:red">Hello World</h1>'); // HTML ichidagi qo'shtirnoqlarni to'g'ri yozish
+    res.render('harid'); // index.ejs ni render qilish
 });
 
 
-const server = http.createServer(app); // serverni yaratamiz
-let PORT = 3000;
+app.post('/create-item', (req, res)=>{
+    console.log(req.body);
+    res.json({test: 'success'});
+});
 
+const server = http.createServer(app);
+let PORT = 4000;
 server.listen(PORT, function () {
     console.log('Server is running on PORT: ' + PORT);
 });
+
+
+// consolda korish
+console.log(__dirname,"bu dirname yonalishi boladi");
+console.log(path.join(__dirname, 'person' , "bu dirname person yonalishi boladi"));
+app.use((req, res, next) => {
+    console.log(`Requested route: ${req.originalUrl}`); // Qaysi route so'rov qilinganligini ko'rsatish
+    next(); // Keyingi middleware'ga o'tish
+});
+
+
