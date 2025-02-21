21e702768a4b92cb0f046d911187488d57105133
sherzod992 <sherzodbek.sharobiddinov1@gmail.com>
2025-02-21 14:39:55 +0900

    Initial commit on author branch

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-console.log('Server-side code running');
+console.log('Web serverni boshlash');
 
 const express = require('express');
 const app = express();
 const http = require('http');
-const path = require('path');
+const res = require('express/lib/response');
+const fs = require('fs');
+
+
+let user;
+fs.readFile("databese/user.json", "utf8", (err, data) => {
+    if(err){
+        console.log("ERROR",err);
+    }else{
+        user = JSON.parse(data);
+    }
+})
 
 // Static fayllar uchun public papkani ko'rsatish
 app.use(express.static('public'));
@@ -14,30 +25,19 @@ app.use(express.urlencoded({ extended: true }));
 app.set("views", "views");
 app.set('view engine', 'ejs');
 
-// Asosiy sahifa
+app.post('/create-item', (req, res)=>{
+});
+app.get("/author", (req, res) => {
+    res.render('author', {user: user});
+});
+
 app.get('/', (req, res) => {
     res.render('harid'); // index.ejs ni render qilish
 });
 
 
-app.post('/create-item', (req, res)=>{
-    console.log(req.body);
-    res.json({test: 'success'});
-});
-
 const server = http.createServer(app);
-let PORT = 4000;
+let PORT = 3000;
 server.listen(PORT, function () {
     console.log('Server is running on PORT: ' + PORT);
 });
-
-
-// consolda korish
-console.log(__dirname,"bu dirname yonalishi boladi");
-console.log(path.join(__dirname, 'person' , "bu dirname person yonalishi boladi"));
-app.use((req, res, next) => {
-    console.log(`Requested route: ${req.originalUrl}`); // Qaysi route so'rov qilinganligini ko'rsatish
-    next(); // Keyingi middleware'ga o'tish
-});
-
-
