3caeec23c435479afff707173fc3af0ab257efd2
sherzod992 <sherzodbek.sharobiddinov1@gmail.com>
2025-02-24 02:07:08 +0900

    train.js yasaldi

--- server.js
+++ server.js
@@ -11,32 +11,37 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
   }
 });
 
-// Express ilovasini yaratish
+//app
 const app = express();
+// 1 express kirish code
 app.use(express.static("public"));
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// View engine sozlash
+//2 sessions code
+//3 views code
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-// Routing
+//4 routing code
+//form in harid has action sending it to /create-item
 app.post("/create-item", (req, res) => {
   res.json({ test: "success" });
 });
 
+///main page rendering harid.ejs in views
 app.get("/", (req, res) => {
   res.render("harid");
 });
 
+// author page route
 app.get("/author", (req, res) => {
   res.render("author", { user: user });
 });
 
-// Server yaratish
+//creating server with https
 let PORT = 3000;
 const server = http.createServer(app);
 server.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
+  console.log(`this app is running in port: ${PORT}`);
 });
---
+++ train.js
@@ -0,0 +1,93 @@
+const list = [
+    "Yaxshi talaba bo`ling",
+    "Tog`ri boshliq tanlang va ko`proq o`qing",
+    "O`zingizning ishlaringizni boshlang",
+    "Siz kuchli bo`lgan narsalarni qiling",
+    "Yoshlarga investitsiya qiling",
+    "Endi dam oling, foydasi yo`q",
+];
+
+function maslahatBering(age, callback) {
+    if (typeof age !== "number") return callback("Insert number", null);
+    if (age <= 20) callback(null, list[0]);
+    else if (age > 20 && age <= 30) callback(null, list[1]);
+    else if (age > 30 && age <= 40) callback(null, list[2]);
+    else if (age > 40 && age <= 50) callback(null, list[3]);
+    else if (age > 50 && age <= 60) callback(null, list[4]);
+    else {
+        setTimeout(() => {
+            callback(null, list[5]);
+        }, 5000);
+    }
+}
+
+console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ --- 1");
+maslahatBering(65, (err, data) => {
+    if (err) console.log("Error:", err);
+    else {
+        console.log("Javob:", data);
+    }
+});
+
+console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ --- 2");
+
+async function giveAsyncAdvice(age) {
+    if (typeof age !== "number") throw new Error("Insert number");
+    else if (age <= 20) return list[0];
+    else if (age > 20 && age <= 30) return list[1];
+    else if (age > 30 && age <= 40) return list[2];
+    else if (age > 40 && age <= 50) return list[3];
+    else if (age > 50 && age <= 60) return list[4];
+    else {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(list[5]);
+            }, 5000);
+        });
+    }
+}
+
+console.log("----------------1");
+giveAsyncAdvice(25)
+    .then((data) => console.log("async answer:", data))
+    .catch(console.error);
+console.log("----------------2");
+
+async function giveAnswer() {
+    try {
+        let answer = await giveAsyncAdvice(20);
+        console.log("with await: ", answer);
+        answer = await giveAsyncAdvice(90);
+        console.log("with await: ", answer);
+        answer = await giveAsyncAdvice(40);
+        console.log("with await: ", answer);
+        answer = await giveAsyncAdvice(50);
+        console.log("with await: ", answer);
+        answer = await giveAsyncAdvice(60);
+        console.log("with await: ", answer);
+    } catch (err) {
+        console.error(err);
+    }
+}
+giveAnswer();
+
+function giveAdvice(age, callback) {
+    if (typeof age !== "number") callback("Insert number", null);
+    else if (age <= 20) callback(null, list[0]);
+    else if (age > 20 && age <= 30) callback(null, list[1]);
+    else if (age > 30 && age <= 40) callback(null, list[2]);
+    else if (age > 40 && age <= 50) callback(null, list[3]);
+    else if (age > 50 && age <= 60) callback(null, list[4]);
+    else {
+        setInterval(() => {
+            callback(null, list[5]);
+        }, 1000);
+    }
+}
+
+giveAdvice(65, (err, data) => {
+    if (err) console.log("error:", err);
+    else {
+        console.log("answer:", data);
+    }
+});
